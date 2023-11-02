02773e1382de01ddc3130fd3c65eafa2d8ae9fea
kangleian <rayhannabhanlukmansyah@gmail.com>
2023-11-02 07:17:56 +0700

    minggu 1 selesai

---
+++ belajar ngoding/minggu1/ujian/ujian2.js
@@ -0,0 +1,48 @@
+// Problem
+// Diberikan sebuah function konversiMenit(menit) yang menerima satu parameter berupa angka yang merupakan ukuran waktu 
+// dalam menit. Function akan me-return string waktu dalam format jam:menit berdasarkan menit tersebut. 
+// Contoh, jika menit adalah 63, maka function akan me-return "1:03".
+
+function konversiMenit(menit) {
+    // you can only write your code here!
+    return `${menit/60}:${menit%60}`
+}
+  
+  // TEST CASES
+  console.log(konversiMenit(63)); // 1:03
+  console.log(konversiMenit(124)); // 2:04
+  console.log(konversiMenit(53)); // 0:53
+  console.log(konversiMenit(88)); // 1:28
+  console.log(konversiMenit(120)); // 2:00
+
+// ------------------------------------
+console.log('')
+// ------------------------------------
+
+//   Problem
+//   Diberikan sebuah function xo(str) yang menerima satu parameter berupa string. 
+//   Function akan me-return true jika jumlah karakter x sama dengan jumlah karakter o, dan false jika tidak.
+
+function xo(str) {
+    // you can only write your code here!
+    let o = 0, x = 0;
+    str.split('').forEach(char => {
+        if(char == 'o'){
+            o++;
+        }else if(char == 'x'){
+            x++;
+        }
+    });
+    if(o == x){
+        return true
+    }else{
+        return false
+    }
+}
+  
+  // TEST CASES
+  console.log(xo('xoxoxo')); // true
+  console.log(xo('oxooxo')); // false
+  console.log(xo('oxo')); // false
+  console.log(xo('xxxooo')); // true
+  console.log(xo('xoxooxxo')); // true
